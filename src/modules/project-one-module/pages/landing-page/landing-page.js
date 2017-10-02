910ec804f1ae79c7aac26fd1be355145073f2268
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2017-10-03 06:23:19 +0800

    added string
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

--- src/modules/project-one-module/pages/landing-page/landing-page.js
+++ src/modules/project-one-module/pages/landing-page/landing-page.js
@@ -6,10 +6,63 @@ import './landing-page.html'
 class LandingPage extends Polymer.Element {
   static get is () { return 'landing-page' }
 
+  static get properties () {
+    return {
+      pin: {
+        type: String,
+        observer: '_onPinChanged'
+      },
+      formattedPin: {
+        type: String
+      }
+    }
+  }
+
   submit () {
-    var pin = this.shadowRoot.querySelector('#pin').value
-    window.history.pushState({}, '', '/page/' + pin)
+    window.history.pushState({}, '', '/page/' + this.pin)
     window.dispatchEvent(new CustomEvent('location-changed'))
+  }
+
+  _onValueChanged (e) {
+
+  }
+
+  _onPinChanged (value, oldValue) {
+    if (oldValue === undefined) {
+      return
+    }
+    if (this.formattedPin === value) {
+      return
+    }
+
+    value = value.replace(/-/g, '')
+    value = value.replace(/\s+/g, '')
+
+    var formattedValue = ''
+    for (var i = 0; i < value.length; i++) {
+      if (i === 3) {
+        formattedValue += '-'
+      }
+
+      if (i === 5) {
+        formattedValue += '-'
+      }
+
+      if (i === 8) {
+        formattedValue += '-'
+      }
+
+      if (i === 10) {
+        formattedValue += '-'
+      }
+      formattedValue += value[i]
+    }
+
+    if (formattedValue.length > 17) {
+      formattedValue = formattedValue.substr(0, 17)
+    }
+    this.formattedPin = formattedValue
+    this.pin = formattedValue
   }
 }
 
