ecae7eaa98a59e16cce2184147690a428748bb3c
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2017-10-03 01:44:15 +0800

    added stuff
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

--- src/modules/project-one-module/pages/page-one/page-one.js
+++ src/modules/project-one-module/pages/page-one/page-one.js
@@ -1,7 +1,10 @@
 import 'polymer/polymer.html'
 import 'paper-input/paper-input.html'
 import 'paper-button/paper-button.html'
+import 'marked-element/marked-element.html'
+import marked from 'marked'
 import './page-one.html'
+window.marked = window.marked || marked
 
 class PageOne extends Polymer.Element {
   static get is () { return 'page-one' }
